740e30e05a802eb1feb78bb9c578cf06e6efdd7c
Guilherme Vitorino da Silva <gui_vito@hotmail.com>
2023-08-21 17:21:31 -0300

    first commit

---
+++ src/test/java/com/spring/batch/v5/SpringBatchV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.batch.v5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
